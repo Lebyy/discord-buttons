96e824ad771ce245be86ed6178e06d89f7398d47
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2021-05-25 09:56:47 +0300

    2.3.2

---
+++ src/Classes/WebhookClient.js
@@ -0,0 +1,36 @@
+const { WebhookClient } = require('discord.js');
+const APIMessage = require('./APIMessage').APIMessageMain;
+
+module.exports = class extends WebhookClient {
+    async editMessage(message, content, options = {}) {
+        const { data, files } = await (
+            content.resolveData?.() ?? APIMessage.create(this, content, options).resolveData()
+        ).resolveFiles();
+        const d = await this.client.api
+            .webhooks(this.id, this.token)
+            .messages(typeof message === 'string' ? message : message.id)
+            .patch({ data, files });
+
+        const messageManager = this.client.channels?.cache.get(d.channel_id)?.messages;
+        if (!messageManager) return d;
+
+        const existing = messageManager.cache.get(d.id);
+        if (!existing) return messageManager.add(d);
+
+        const clone = existing._clone();
+        clone._patch(d);
+        return clone;
+    }
+
+    async deleteMessage(message) {
+        await this.client.api
+            .webhooks(this.id, this.token)
+            .messages(typeof message === 'string' ? message : message.id)
+            .delete();
+    }
+
+    async fetchMessage(message, cache = true) {
+        const data = await this.client.api.webhooks(this.id, this.token).messages(message).get();
+        return this.client.channels?.cache.get(data.channel_id)?.messages.add(data, cache) ?? data;
+    }
+}
