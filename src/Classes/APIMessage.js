c91f9428e77dd58fc8f492d80147690409b30b93
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2021-05-25 09:59:46 +0300

    2.3.2

--- src/Classes/APIMessage.js
+++ src/Classes/APIMessage.js
@@ -2,6 +2,45 @@ const { APIMessage } = require("discord.js");
 const Util = require('../Util');
 const MessageButton = require('./MessageButton');
 
+class sendAPICallback extends APIMessage {
+    resolveData() {
+
+        if (this.data) {
+            return this;
+        }
+
+        super.resolveData();
+
+        if (this.options.flags) {
+            this.data.flags = parseInt(this.options.flags);
+        }
+
+        if (typeof (this.options.ephemeral) === 'boolean' && this.options.ephemeral === true) {
+            this.data.flags = 64;
+        }
+
+        const buttons = [];
+        if (this.options.type === 2) {
+            buttons.push(Util.resolveButton(this.options));
+        } else if (this.options.buttons) {
+            this.options.buttons.map((x) => buttons.push(Util.resolveButton(x)));
+        } else if (this.options.button) {
+            buttons.push(Util.resolveButton(this.options.button));
+        }
+
+        if (buttons.length) {
+            this.data.components = [
+                {
+                    type: 1,
+                    components: buttons
+                }
+            ];
+        }
+
+        return this;
+    }
+}
+
 class APIMessageMain extends APIMessage {
     resolveData() {
         if (this.data) {
@@ -10,16 +49,14 @@ class APIMessageMain extends APIMessage {
 
         super.resolveData();
 
-        const buttonLikes = [];
+        const buttons = [];
         if (this.options.type === 2) {
-            buttonLikes.push(Util.resolveButton(this.options));
+            buttons.push(Util.resolveButton(this.options));
         } else if (this.options.buttons) {
-            this.options.buttons.map((x) => buttonLikes.push(Util.resolveButton(x)));
+            this.options.buttons.map((x) => buttons.push(Util.resolveButton(x)));
         } else if (this.options.button) {
-            buttonLikes.push(Util.resolveButton(this.options.button));
+            buttons.push(Util.resolveButton(this.options.button));
         }
-
-        const buttons = buttonLikes.map(b => new MessageButton(b).toJSON());
 
         if (buttons.length) {
             this.data.components = [
@@ -35,5 +72,6 @@ class APIMessageMain extends APIMessage {
 }
 
 module.exports = {
+    sendAPICallback,
     APIMessageMain
 }
