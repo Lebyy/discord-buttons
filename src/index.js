c262d36774a1b478d0beb229d5510726f26619fb
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2021-05-25 09:55:00 +0300

    2.3.2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const Message = require('./Classes/Message');
 const TextChannel = require('./Classes/TextChannel');
 const DMChannel = require('./Classes/DMChannel');
 const MessageButton = require('./Classes/MessageButton');
-const INTERACTION_CREATE = require('./Events/INTERACTION_CREATE');
+const INTERACTION_CREATE = require('./Classes/INTERACTION_CREATE.js');
 
 module.exports = (client) => {
 
@@ -15,7 +15,16 @@ module.exports = (client) => {
     Structures.extend("TextChannel", () => TextChannel);
     Structures.extend("DMChannel", () => DMChannel);
 
-    client.ws.on('INTERACTION_CREATE', data => INTERACTION_CREATE(client, data))
+    client.ws.on('INTERACTION_CREATE', data => {
+
+        if (!data.message) return;
+
+        if (data.data.component_type === 2) {
+            const button = new INTERACTION_CREATE(client, data);
+
+            client.emit('clickButton', button);
+        }
+    });
 
     return {
         MessageButton: MessageButton
