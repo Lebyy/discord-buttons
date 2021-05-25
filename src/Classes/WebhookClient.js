e31980c3fd2d852f1c69d99f6cc2631be0f90918
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2021-05-25 10:08:23 +0300

    2.3.3

--- src/Classes/WebhookClient.js
+++ src/Classes/WebhookClient.js
@@ -3,15 +3,14 @@ const APIMessage = require('./APIMessage').APIMessageMain;
 
 module.exports = class extends WebhookClient {
     async editMessage(message, content, options = {}) {
-        const { data, files } = await (
-            content.resolveData?.() ?? APIMessage.create(this, content, options).resolveData()
+        const { data, files } = await (content instanceof APIMessage ? content : APIMessage.create(this, content, options).resolveData()
         ).resolveFiles();
         const d = await this.client.api
             .webhooks(this.id, this.token)
             .messages(typeof message === 'string' ? message : message.id)
             .patch({ data, files });
 
-        const messageManager = this.client.channels?.cache.get(d.channel_id)?.messages;
+        const messageManager = this.client.channels ? (this.channels.cache.get(d.channel_id) ? this.channels.cache.get(d.channel_id).messages : null) : null;
         if (!messageManager) return d;
 
         const existing = messageManager.cache.get(d.id);
@@ -31,6 +30,6 @@ module.exports = class extends WebhookClient {
 
     async fetchMessage(message, cache = true) {
         const data = await this.client.api.webhooks(this.id, this.token).messages(message).get();
-        return this.client.channels?.cache.get(data.channel_id)?.messages.add(data, cache) ?? data;
+        return this.client.channels ? (this.client.channels.cache.get(data.channel_id) ? this.client.channels.cache.get(data.channel_id).messages.add(data, cache) : null) : undefined ?? data;
     }
 }
