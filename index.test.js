8cb97bb44c392f6dbc65188a5fa667ad124681ca
Angelo II <62988428+AngeloCore@users.noreply.github.com>
2021-06-02 20:36:17 +0300

    3.1.0-dev

--- index.test.js
+++ index.test.js
@@ -1,21 +1,27 @@
 const discord = require('discord.js');
-const client = new discord.Client()
+const client = new discord.Client({ intents: discord.Intents.ALL })
 const disbut = require('./src/index');
 disbut(client)
 const Util = require('./src/v12/Util');
 
-client.on('ready', () => console.log(client.user.tag));
+client.on('ready', () => {
+    console.log(client.user.tag)
+    client.guilds.cache.get('728503987866828870').commands.create({ name: 'hi', 'description': 'HI' });
+});
 //client.on('debug', console.log);
 
-client.on('message', async message => {
-    if (message.author.bot) return;
+client.on('message', async (message) => {
+    //if (message.author.bot) return;
     if (message.content.startsWith('o')) {
 
+        let e = message.guild.emojis.cache.get('729208650131963946');
+
         let btn = new disbut.MessageButton()
-            .setEmoji('785062885952192512')
+            .setEmoji(e)
             .setStyle('gray')
             .setID('testid')
-            .setEmoji('535993902180859914')
+
+        console.log(btn)
 
         let group1 = new disbut.MessageActionRow()
             .addComponent(btn)
@@ -27,9 +33,9 @@ client.on('message', async message => {
 
         let m = await message.channel.send(`Wumpus!!!`, { component: group1 });
 
-        await wait(1000);
+        //await wait(1000);
 
-        m.edit('yea', { component: null });
+        //m.edit('yea', { component: null });
     } else if (message.content.startsWith('s')) {
         message.channel.send('sbuwbdsjndsjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdnkalsnsnsjne dncmsdbh f nrjhknbhnkjrsfnhsdn  rjrjrjrrrrrrrrrrrrrrrrrrrriojaiudhfurjsbvisnfadhiuhsiuzdravei az se kazvam angelo priqtno mi e hahadajidsjidjaisssssssssssssssssssssjuhuifsbebdhsfhdsbfhwbahfdbfbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbwuiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiibbbbbbbbbbbbbbbbbbbbqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqiiiiiiiiiiiiiiiiiiiiiiiiiibbbdkkkkkkkkkkkkkkkkkkkiwhqksjdhbcuaaaaaaaaaaaaaaaaaaaaaaaaaaudhdncbdjsjooooooooooooooooooooooooooooqqqqqqqqqqqqwwwwwwwwwwwwwwwweeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrttttttttttttttttyyyyyyyyyyyyyyyyuuuuuuuuuuuuuiooooooooooooooooppppppppppppppppppppaaaaaaaaaaasssssssssbbbbbbbbdksndjjjjjjjjjjjjjjjjjjsssssssssssssssssssssssssssssssssadwdabhsbsdgysdhagdagyydihbsbgggggdyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeedbjskjjddddddddddddddddddddddddddddddddddddddddddddddddduwebsddusiudijbuibiabdiugdebiuwbwaibewuafgwifuggggggggggggggggggggggggggggggggggggggggggssssssssssssssssssooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooohsaifhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhweuiooooooooooooooooooooooooooooooooopap\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaufhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhheuwfuuuuuuuuuuuuuuuuuuuuuuuuuuuuusbuwbdsjndsjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjdnkalsnsnsjne dncmsdbh f nrjhknbhnkjrsfnhsdn  rjrjrjrrrrrrrrrrrrrrrrrrrriojaiudhfurjsbvisnfadhiuhsiuzdravei az se kazvam angelo priqtno mi e hahadajidsjidjaisssssssssssssssssssssjuhuifsbebdhsfhdsbfhwbahfdbfbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbwuiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiibbbbbbbbbbbbbbbbbbbbqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqiiiiiiiiiiiiiiiiiiiiiiiiiibbbdkkkkkkkkkkkkkkkkkkkiwhqksjdhbcuaaaaaaaaaaaaaaaaaaaaaaaaaaudhdncbdjsjooooooooooooooooooooooooooooqqqqqqqqqqqqwwwwwwwwwwwwwwwweeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrttttttttttttttttyyyyyyyyyyyyyyyyuuuuuuuuuuuuuiooooooooooooooooppppppppppppppppppppaaaaaaaaaaasssssssssbbbbbbbbdksndjjjjjjjjjjjjjjjjjjsssssssssssssssssssssssssssssssssadwdabhsbsdgysdhagdagyydihbsbgggggdyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeedbjskjjddddddddddddddddddddddddddddddddddddddddddddddddduwebsddusiudijbuibiabdiugdebiuwbwaibewuafgwifuggggggggggggggggggggggggggggggggggggggggggssssssssssssssssssooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooohsaifhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhweuiooooooooooooooooooooooooooooooooopapaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaufhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhheuwfuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', {
             tts: true,
@@ -57,11 +63,24 @@ client.on('clickButton', async (button) => {
         .setStyle('blurple');
 
     let row = new disbut.MessageActionRow()
-    .addComponent(btn)
+        .addComponent(btn)
 
     await button.channel.send('hi', { components: row });
     await button.reply.send('hi', { components: row });
 });
+
+client.on('interaction', async (interaction) => {
+    let btn = new disbut.MessageButton()
+    .setEmoji('785062885952192512')
+    .setID('d')
+    .setStyle('blurple');
+
+    interaction.user.send('GG', btn);
+
+    interaction.reply('hi');
+    await wait(1000);
+    interaction.editReply('hi', btn);
+})
 
 client.login('');
 
--- src/v13/Classes/MessageButton.js
+++ src/v13/Classes/MessageButton.js
@@ -1,7 +1,7 @@
-const { MessageComponentTypes } = require('../Constants.js');
-const BaseMessageComponent = require('./interfaces/BaseMessageComponent');
-const { resolveString } = require('discord.js').Util;
-const { resolveStyle, isEmoji } = require('../Util');
+const { MessageComponentTypes } = require('../../v12/Constants');
+const BaseMessageComponent = require('../../v12/Classes/interfaces/BaseMessageComponent');
+const { verifyString } = require('discord.js').Util;
+const { resolveStyle, isEmoji } = require('../../v12/Util');
 
 class MessageButton extends BaseMessageComponent {
 
@@ -14,14 +14,14 @@ class MessageButton extends BaseMessageComponent {
 
         this.style = 'style' in data ? resolveStyle(data.style) : null;
 
-        this.label = ('label' in data && data.label) ? resolveString(data.label) : undefined;
+        this.label = ('label' in data && data.label) ? verifyString(data.label) : undefined;
 
         this.disabled = 'disabled' in data ? data.disabled : false;
 
         this.emoji = 'emoji' in data ? data.emoji : undefined;
 
         if ('url' in data && data.url)
-            this.url = resolveString(data.url)
+            this.url = verifyString(data.url)
         else this.url = undefined;
 
         if (('id' in data && data.id) || ('custom_id' in data && data.custom_id))
@@ -38,7 +38,7 @@ class MessageButton extends BaseMessageComponent {
     }
 
     setLabel(label) {
-        label = resolveString(label);
+        label = verifyString(label);
         this.label = label;
         return this;
     }
@@ -49,20 +49,20 @@ class MessageButton extends BaseMessageComponent {
     }
 
     setURL(url) {
-        this.url = resolveString(url);
+        this.url = verifyString(url);
         return this;
     }
 
     setID(id) {
-        this.custom_id = resolveString(id);
+        this.custom_id = verifyString(id);
         return this;
     }
 
-    setEmoji(emoji) {
+    setEmoji(emoji, animated) {
         if (!emoji) return this;
-        if (isEmoji(emoji) === true) this.emoji = { name: resolveString(emoji) }
+        if (isEmoji(emoji) === true) this.emoji = { name: verifyString(emoji) }
         else if (emoji.id) this.emoji = { id: emoji.id }
-        else if (resolveString(emoji).length > 0) this.emoji = { id: resolveString(emoji) }
+        else if (verifyString(emoji).length > 0) this.emoji = { id: verifyString(emoji) }
         else this.emoji = { name: null, id: null };
         if ((animated && typeof (animated) !== 'boolean') || (emoji.animated && typeof (emoji.animated) !== 'boolean')) throw new SyntaxError('The emoji animated option must be boolean');
         if (this.emoji && typeof (emoji.animated) === 'boolean') this.emoji.animated = emoji.animated;
