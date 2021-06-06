86c77d4e18ecc9f2185f2bb54039274fba0e3498
Lebyy <45114019+Lebyy@users.noreply.github.com>
2021-06-06 14:28:28 +0530

    📝Fixing types

--- typings/Events/INTERACTION_CREATE.d.ts
+++
@@ -1,3 +0,0 @@
-declare function _exports(client: Client, data: object): Promise<void>;
-export = _exports;
-import { Client } from "discord.js";
--- typings/v12/Util.d.ts
+++ typings/v12/Util.d.ts
@@ -1,14 +1,4 @@
 export function resolveStyle(style: any): any;
-export function resolveStyle(style: any): any;
-export function resolveButton(data: any): {
-    style: any;
-    label: any;
-    emoji: any;
-    disabled: any;
-    url: any;
-    custom_id: any;
-    type: number;
-};
 export function resolveButton(data: any): {
     style: any;
     label: any;
@@ -19,6 +9,4 @@ export function resolveButton(data: any): {
     type: number;
 };
 export function resolveType(type: any): any;
-export function resolveType(type: any): any;
-export function isEmoji(string: any): boolean;
 export function isEmoji(string: any): boolean;
